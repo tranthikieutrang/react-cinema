22feb193dad39d33b4489d6d16ac77ab349bb052
nam.do <nam.do@elsanow.io>
2022-11-12 21:19:30 +0700

    init project cinema

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+import AutoScrollToTop from './components/AutoScrollToTop';
+import {
+  BookingTickets,
+  Home,
+  MoviesDetails,
+  SignIn,
+  SignUp,
+  News,
+  Events,
+  AboutUs,
+  Profile,
+  MoviesManagement,
+  UserManagement,
+  UnderMaintenance,
+  AddAndEditUsers,
+  AddAndEditMovies,
+  AddShowTime,
+  PageNotFound,
+} from './pages';
+import HomeLayout from './sharedLayout/HomeLayout';
+import AdminLayout from './sharedLayout/AdminLayout';
+import NoUserProtected from './protectedRoutes/NoUserProtected';
+import UserProtected from './protectedRoutes/UserProtected';
+import AdminProtected from './protectedRoutes/AdminProtected';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+function App() {
+  const { isLoading } = useSelector((state) => state.ui);
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      {isLoading && <LoadingSpinner />}
+      <AutoScrollToTop />
+      <ToastContainer theme='dark' autoClose={3000} />
+      <Routes>
+        <Route path='/' element={<HomeLayout />}>
+          <Route index element={<Home />} />
+          <Route path='movies/:id' element={<MoviesDetails />} />
+          <Route path='news' element={<News />} />
+          <Route path='events' element={<Events />} />
+          <Route path='about-us' element={<AboutUs />} />
+          <Route element={<UserProtected />}>
+            <Route path='showTime/:id' element={<BookingTickets />} />
+            <Route path='profile' element={<Profile />} />
+          </Route>
+        </Route>
+        <Route element={<NoUserProtected />}>
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-in' element={<SignIn />} />
+        </Route>
+        <Route element={<AdminProtected />}>
+          <Route path='/admin' element={<AdminLayout />}>
+            <Route index element={<UserManagement />} />
+            <Route path='add-edit-user' element={<AddAndEditUsers />} />
+            <Route path='movies-management' element={<MoviesManagement />} />
+            <Route path='add-edit-movie' element={<AddAndEditMovies />} />
+            <Route path='add-showtime' element={<AddShowTime />} />
+            <Route path='support' element={<UnderMaintenance />} />
+            <Route path='report-bug' element={<UnderMaintenance />} />
+          </Route>
+        </Route>
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import BookingTickets from './BookingTickets';
+import Home from './Home';
+import MoviesDetails from './MoviesDetails';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import News from './News';
+import Events from './Events';
+import AboutUs from './AboutUs';
+import Profile from './Profile';
+import MoviesManagement from './Admin/MoviesManagement';
+import UserManagement from './Admin/UserManagement';
+import UnderMaintenance from './Admin/UnderMaintenance';
+import AddAndEditUsers from './Admin/AddAndEditUsers';
+import AddAndEditMovies from './Admin/AddAndEditMovies';
+import AddShowTime from './Admin/AddShowTime';
+import PageNotFound from './PageNotFound';
+
+export {
+  BookingTickets,
+  Home,
+  MoviesDetails,
+  SignIn,
+  SignUp,
+  News,
+  Events,
+  AboutUs,
+  Profile,
+  MoviesManagement,
+  UserManagement,
+  UnderMaintenance,
+  AddAndEditUsers,
+  AddAndEditMovies,
+  AddShowTime,
+  PageNotFound,
+};
